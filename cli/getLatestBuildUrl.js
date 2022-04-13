451b5fd6d4d4f3a856ae5464c963b2d444b557d7
Matthias <matthias.fischer@lingvano.com>
2022-04-13 16:06:05 +0200

    write build url to file

--- cli/getLatestBuildUrl.js
+++ cli/getLatestBuildUrl.js
@@ -1,24 +1,21 @@
-const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
-const easCommand =
-  'eas build:list --distribution=simulator --status=finished --platform=ios --limit=1 --json';
+fs.readFile(
+  path.resolve(__dirname, 'buildOutput.txt'),
+  'UTF-8',
+  (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
 
-exec(easCommand, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
+    const buildUrl = data.match(/buildUrl": "(.*)"/)[1];
+    if (buildUrl) {
+      console.log(buildUrl);
+      process.exit(0);
+    }
+
     process.exit(1);
   }
-  if (stderr && !stderr.includes('please upgrade')) {
-    console.log(`stderr: ${stderr}`);
-    process.exit(1);
-  }
-
-  const buildUrl = stdout.match(/buildUrl": "(.*)"/)[1];
-  if (buildUrl) {
-    console.log(buildUrl);
-    process.exit(0);
-  }
-
-  console.log('An unknown error occured');
-  process.exit(1);
-});
+);
